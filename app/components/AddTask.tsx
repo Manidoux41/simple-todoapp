0fdb91e0e73956f5eee753695909a4cbb7edd94c
Manidoux41 <110053179+Manidoux41@users.noreply.github.com>
2023-04-22 10:50:25 +0200

    add onchange and onsubmit function

--- app/components/AddTask.tsx
+++ app/components/AddTask.tsx
@@ -2,20 +2,29 @@
 
 import {AiOutlinePlus} from 'react-icons/ai'
 import Modal from './Modal'
-import { useState } from 'react'
+import { FormEventHandler, useState } from 'react'
 
 const AddTask = () => {
 
   const [modalOpen, setModalOpen] = useState<boolean>(false)
+  const [newTaskValue, setNewTaskValue] = useState<string>('')
+
+  const handleSubmitNewTodo: FormEventHandler<HTMLFormElement> = (e) => {
+    e.preventDefault();
+    console.log(newTaskValue);
+    setNewTaskValue('');
+    
+
+  }
 
   return (
     <div>
         <button onClick={() => setModalOpen(true)} className="btn btn-primary w-full">A new tesk<AiOutlinePlus className='ml-2' size={18}/></button>
         <Modal modalOpen={modalOpen} setModalOpen={setModalOpen}>
-          <form>
+          <form onSubmit={handleSubmitNewTodo}>
             <h3 className='font-bold text-lg'>Add new task</h3>
             <div className='modal-action'>
-            <input type="text" placeholder="Type here" className="input input-bordered w-full" />
+            <input value={newTaskValue} onChange={(e) => setNewTaskValue(e.target.value)} type="text" placeholder="Type here" className="input input-bordered w-full" />
             <button type='submit' className='btn'>Submit</button>
             </div>
           </form>
