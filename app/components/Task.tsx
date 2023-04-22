af15d2cec4c4b41c5b48ae6214f7f423e478e973
Manidoux41 <110053179+Manidoux41@users.noreply.github.com>
2023-04-22 14:35:42 +0200

    add deleted button and function

--- app/components/Task.tsx
+++ app/components/Task.tsx
@@ -4,21 +4,36 @@ import { ITask } from "@/types/tasks";
 import { useState } from "react";
 import { FiEdit, FiTrash2 } from 'react-icons/fi';
 import Modal from "./Modal";
+import { useRouter } from "next/navigation";
+import { deleteTodo, editTodo } from "@/api";
 
 interface TaskProps {
     task: ITask
 }
 
 const Task: React.FC<TaskProps> = ({ task }) => {
-  
+  const router = useRouter();
   const [openModalEdit, setOpenModalEdit] = useState<boolean>(false)
+  const [openModalDeleted, setOpenModalDeleted] = useState<boolean>(false)
   const [openModal, setModal] = useState<boolean>(false)
   const [taskToEdit, setTaskToEdit] = useState<string>(task.text)
+  
 
-  const handleSubmitEditTodo = (e) => {
-    e.preventDefault()
-    console.log(openModalEdit);
-    
+  const handleSubmitEditTodo: FormEventHandler<HTMLFormElement> = async (e) => {
+    e.preventDefault();
+    await editTodo({
+      id: task.id,
+      text: taskToEdit
+    });
+    setTaskToEdit('');
+    setOpenModalEdit(false);
+    router.refresh();
+  }
+
+  const handleDeleteTask = async (id:string) => {
+    await deleteTodo(id);
+    setOpenModalDeleted(false)
+    router.refresh()
   }
 
   return (
@@ -35,7 +50,13 @@ const Task: React.FC<TaskProps> = ({ task }) => {
             </div>
           </form>
         </Modal>
-        <FiTrash2 cursor="pointer" className="text-red-500" size={25} />
+        <FiTrash2 onClick={() => setOpenModalDeleted(true)} cursor="pointer" className="text-red-500" size={25} />
+        <Modal modalOpen={openModalDeleted} setModalOpen={setOpenModalDeleted}>
+          <h3 className="text-lg">Are you sure, you want to delete this task ?</h3>
+          <div className="modal-action">
+            <button onClick={() => handleDeleteTask(task.id)} className="btn">Yes</button>
+          </div>
+        </Modal>
       </td>
     </tr>
   );
